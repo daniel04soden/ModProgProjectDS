88d8a9e5284fa8c755b6f4ad40efd604b2bcd51b
daniel04soden <dsoden09@gmail.com>
2024-03-27 22:12:56 +0000

    Setup the clinic file wiht main,imports and lists

---
+++ clinic.py
@@ -0,0 +1,28 @@
+# Author: Daniel Soden
+# Purpose: Python Clinic Booking Project
+
+# Import Statements
+
+from reusable import *
+
+# Named constants
+
+OPENING_HOUR = 9
+CLOSING_HOUR = 5
+
+# Lists for storing values
+
+booked_dates = []
+booked_times = []
+
+# Clinic Specific Functions
+
+# Main Output
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
